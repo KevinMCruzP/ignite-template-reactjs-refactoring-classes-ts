fc511ba402b05ca784b0c9da9659a20e47701935
Kevin <kevin_mcp@outlook.com>
2022-08-25 18:02:42 -0400

    Conf las paginas a typescript y function component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Routes from "./routes";
+
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  );
+}
+
+export default App;
